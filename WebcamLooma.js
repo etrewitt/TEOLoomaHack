8d0c139a914382cd873ad5f9dc5013688810731d
Erik <etrewitt@users.noreply.github.com>
2016-02-27 13:43:44 -0800

    Got video recording to work
    Not saving yet, but it's good stuff. Will do both snapshots and video
    (and display only the most recent one!).

--- WebcamLooma.js
+++ WebcamLooma.js
@@ -1,34 +1,128 @@
+console.log("test");
+
+var chunks = [];
+
+var constraints = {video: true};
+
 // Put event listeners into place
-		window.addEventListener("DOMContentLoaded", function() {
-			// Grab elements, create settings, etc.
-			var canvas = document.getElementById("myCanvas"),
-				context = canvas.getContext("2d"),
-				video = document.getElementById("video"),
-				videoObj = { "video": true },
-				errBack = function(error) {
-					console.log("Video capture error: ", error.code); 
-				};
+window.addEventListener("DOMContentLoaded", function() {
+    // Grab elements, create settings, etc.
+    var canvas = document.getElementById("stillCanvas"),
+        context = canvas.getContext("2d"),
+        video = document.getElementById("video"),
+        replayer = document.getElementById('videoCanvas');
+        videoObj = { "video": true },
+        errBack = function(error) {
+            console.log("Video capture error: ", error.code); 
+        }
+        onSuccess = function(stream){
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+            
+            console.log("stream", stream);
+            var mediaRecorder = new MediaRecorder(stream);
 
-			// Put video listeners into place
-			if(navigator.getUserMedia) { // Standard
-				navigator.getUserMedia(videoObj, function(stream) {
-					video.src = stream;
-					video.play();
-				}, errBack);
-			} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
-				navigator.webkitGetUserMedia(videoObj, function(stream){
-					video.src = window.webkitURL.createObjectURL(stream);
-					video.play();
-				}, errBack);
-			} else if(navigator.mozGetUserMedia) { // WebKit-prefixed
-				navigator.mozGetUserMedia(videoObj, function(stream){
-					video.src = window.URL.createObjectURL(stream);
-					video.play();
-				}, errBack);
-			}
+            document.getElementById("start").onclick = function() {
+                chunks = [];
+                mediaRecorder.start();
+                console.log("recorder started");
+            }
 
-			// Trigger photo take
-			document.getElementById("snap").addEventListener("click", function() {
-				context.drawImage(video, 0, 0, 640, 480);
-			});
-		}, false);
+            document.getElementById("stop").onclick = function() {
+                mediaRecorder.stop();
+                console.log("recorder stopped");
+                replayer.style.display = "block";
+                document.getElementById("stillCanvas").style.display = "none";
+            }
+
+            mediaRecorder.ondataavailable = function(e) {
+                console.log("data available");
+                chunks.push(e.data);
+            }
+
+            mediaRecorder.onstop = function(e) {
+                console.log('onstop fired');
+                var blob = new Blob(chunks, { 'type' : 'video/ogv; codecs=opus' });
+                replayer.src = window.URL.createObjectURL(blob);
+            };
+
+            mediaRecorder.onwarning = function(e) {
+                console.log('onwarning fired');
+            };
+
+            mediaRecorder.onerror = function(e) {
+                console.log('onerror fired');
+            };
+        };
+
+    // Put video listeners into place
+    if(navigator.getUserMedia) { // Standard
+        console.log("navigator.getUserMedia");
+        navigator.getUserMedia(videoObj, onSuccess, errBack);
+    } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+        console.log("navigator.webkitGetUserMedia");
+        navigator.webkitGetUserMedia(videoObj, onSuccess, errBack);
+    } else if(navigator.mozGetUserMedia) { // WebKit-prefixed
+        console.log("navigator.mozGetUserMedia");
+        navigator.mozGetUserMedia(videoObj, onSuccess, errBack);
+    }
+
+    // Trigger photo take
+    document.getElementById("snap").addEventListener("click", function() {
+        context.drawImage(video, 0, 0, 640, 480);
+        document.getElementById("stillCanvas").style.display = "block";
+        document.getElementById("videoCanvas").style.display = "none";
+    });
+}, false);
+
+var record = document.getElementById('start');
+var stop = document.getElementById('stop');
+var video = document.getElementById('videoCanvas');
+video.setAttribute('controls', '');
+
+var chunks = [];
+
+var constraints = {video: true};
+
+var onSuccess = function(stream) {
+  console.log("stream", stream);
+  var mediaRecorder = new MediaRecorder(stream);
+
+  record.onclick = function() {
+    chunks = [];
+    mediaRecorder.start();
+    console.log("recorder started");
+  }
+
+  stop.onclick = function() {
+    mediaRecorder.stop();
+    console.log("recorder stopped");
+    video.style.display = "block";
+    document.getElementById("stillCanvas").style.display = "none";
+  }
+
+  mediaRecorder.ondataavailable = function(e) {
+    console.log("data available");
+    chunks.push(e.data);
+  }
+
+  mediaRecorder.onstop = function(e) {
+    console.log('onstop fired');
+    var blob = new Blob(chunks, { 'type' : 'video/ogv; codecs=opus' });
+    video.src = window.URL.createObjectURL(blob);
+  };
+
+  mediaRecorder.onwarning = function(e) {
+    console.log('onwarning fired');
+  };
+
+  mediaRecorder.onerror = function(e) {
+    console.log('onerror fired');
+  };
+};
+
+var onError = function(err) {
+  console.log('The following error occured: ' + err);
+}
+
+navigator.getUserMedia(constraints, onSuccess, onError);
