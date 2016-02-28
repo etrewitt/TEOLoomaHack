68b059d71f1393cb5e9b38f075de02650d0362ce
OjusRao <oarsujo@gmail.com>
2016-02-27 21:27:19 -0800

    audio files working

--- WebcamLooma.js
+++ WebcamLooma.js
@@ -1,31 +1,176 @@
+console.log("test");
+
+var chunks = [];
+
+var constraints = {video: true};
+
+var COUNTER = 0;
+
+
+function hasGetUserMedia() {
+  return !!(navigator.getUserMedia || navigator.webkitGetUserMedia ||
+            navigator.mozGetUserMedia || navigator.msGetUserMedia);
+}
+
+if (hasGetUserMedia()) {
+  // Good to go!
+} else {
+  alert('getUserMedia() is not supported in your browser');
+}
+
 // Put event listeners into place
-
 window.addEventListener("DOMContentLoaded", function() {
-	// Grab elements, create settings, etc.
-	var canvas = document.getElementById("mycanvas"),
-		context = canvas.getContext("2d"),
-		video = document.getElementById("video"),
-		videoObj = { "video": true },
-		errBack = function(error) {
-			console.log("Video capture error: ", error.code); 
-		};
+   
+    // Grab elements, create settings, etc.
+    var canvas = document.getElementById("stillCanvas"),
+        context = canvas.getContext("2d"),
+        video = document.getElementById("video"),
+        replayer = document.getElementById('videoCanvas');
+     
+        videoObj = { "video": true, "audio": true },
+       
+        errBack = function(error) {
+            console.log("Video capture error: ", error.code); 
+        }
+        onSuccess = function(stream){
+            video.src = window.URL.createObjectURL(stream);
+            video.play();
+            
 
-	// Put video listeners into place
-	if(navigator.getUserMedia) { // Standard
-		navigator.getUserMedia(videoObj, function(stream) {
-			video.src = stream;
-			video.play();
-		}, errBack);
-	} else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
-		navigator.webkitGetUserMedia(videoObj, function(stream){
-			video.src = window.webkitURL.createObjectURL(stream);
-			video.play();
-		}, errBack);
-	}
-	else if(navigator.mozGetUserMedia) { // Firefox-prefixed
-		navigator.mozGetUserMedia(videoObj, function(stream){
-			video.src = window.URL.createObjectURL(stream);
-			video.play();
-		}, errBack);
-	}
+   
+            console.log("stream", stream);
+            var mediaRecorder = new MediaRecorder(stream);
+
+            document.getElementById("start").onclick = function() {
+                chunks = [];
+                mediaRecorder.start();
+                console.log("recorder started");
+            }
+        
+			
+    		document.getElementById("stop").onclick = function() {
+                mediaRecorder.stop();
+                console.log("recorder stopped");
+                replayer.style.display = "block";
+                document.getElementById("stillCanvas").style.display = "none";
+            }
+            
+            document.getElementById("save").onclick = function() {
+                COUNTER = COUNTER + 1;
+                var uri = canvas.toDataURL();
+                console.log(uri);
+                localStorage.setItem(COUNTER, uri.toString());
+                
+            }
+            
+            document.getElementById("view").onclick = function() {
+                console.log(localStorage.getItem(COUNTER));
+            };
+            
+            document.getElementById("video").load();
+
+            mediaRecorder.ondataavailable = function(e) {
+                console.log("data available");
+                chunks.push(e.data);
+            };
+
+            mediaRecorder.onstop = function(e) {
+                console.log('onstop fired');
+                var blob = new Blob(chunks, { 'type' : 'video/ogv; codecs=opus' });
+                replayer.src = window.URL.createObjectURL(blob);
+            };
+
+			document.getElementById("video").onclick = function() {
+				if (typeof video.loop == 'boolean') {
+				 // loop supported
+			  video.loop = true;
+				} 
+			}
+			
+			
+            mediaRecorder.onwarning = function(e) {
+                console.log('onwarning fired');
+            };
+
+            mediaRecorder.onerror = function(e) {
+                console.log('onerror fired');
+            };
+        
+	};
+    // Put video listeners into place
+    if(navigator.getUserMedia) { // Standard
+        console.log("navigator.getUserMedia");
+        navigator.getUserMedia(videoObj, onSuccess, errBack);
+    } else if(navigator.webkitGetUserMedia) { // WebKit-prefixed
+        console.log("navigator.webkitGetUserMedia");
+        navigator.webkitGetUserMedia(videoObj, onSuccess, errBack);
+    } else if(navigator.mozGetUserMedia) { // WebKit-prefixed
+        console.log("navigator.mozGetUserMedia");
+        navigator.mozGetUserMedia(videoObj, onSuccess, errBack);
+    }
+
+    // Trigger photo take
+    document.getElementById("snap").addEventListener("click", function() {
+        context.drawImage(video, 0, 0, 640, 480);
+        document.getElementById("stillCanvas").style.display = "block";
+        document.getElementById("videoCanvas").style.display = "none";
+    });
 }, false);
+
+var record = document.getElementById('start');
+var stop = document.getElementById('stop');
+var video = document.getElementById('videoCanvas');
+
+video.setAttribute('controls', '');
+
+var chunks = [];
+
+var constraints = {video: true};
+
+var onSuccess = function(stream) {
+  console.log("stream", stream);
+  var mediaRecorder = new MediaRecorder(stream);
+
+  record.onclick = function() {
+    chunks = [];
+    mediaRecorder.start();
+    console.log("recorder started");
+  }
+
+  stop.onclick = function() {
+    mediaRecorder.stop();
+    console.log("recorder stopped");
+    video.style.display = "block";
+    document.getElementById("stillCanvas").style.display = "none";
+  }
+  
+ /* play.onclick = function (){
+    mediaRecorder.play();
+    console.log("recorder playing");
+    document.getElementById("videoCanvas").style.display = "block";
+  }
+*/
+  mediaRecorder.ondataavailable = function(e) {
+    console.log("data available");
+    chunks.push(e.data);
+  }
+
+  mediaRecorder.onstop = function(e) {
+    console.log('onstop fired');
+    var blob = new Blob(chunks, { 'type' : 'video/ogv; codecs=opus' });
+    video.src = window.URL.createObjectURL(blob);
+  };
+
+  
+  mediaRecorder.onwarning = function(e) {
+    console.log('onwarning fired');
+  };
+
+  mediaRecorder.onerror = function(e) {
+    console.log('onerror fired');
+  };
+};
+
+var onError = function(err) {
+  console.log('The following error occured: ' + err);
+}
